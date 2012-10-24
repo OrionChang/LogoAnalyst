787cf3090bc922842828b53c06a7a87494045c58
Orion Zhang <psm.zhang@yahoo.com>
2012-10-24 16:15:22 +0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LOGOAnalyst::Application.config.secret_token = 'c22988b3629cfd33f1643cbb2822121edf7a72a7ff6411ad6c18efcd688448988d1506738323199fa320a21fe08600481d7d413b6f639b42d29522554e395f57'
